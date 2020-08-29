fff9faf4c7c3eae9a155b05085a7041e54e076fa
Dmitry <donveds@gmail.com>
2020-08-29 15:29:40 +0300

    Home page start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="Home">
+    <!-- <AICard/>
+    { #each PlantsArr as plant }
+      <PlantCard plantName="{plant.plantName}"/>
+    { /each }
+    <NewPlantCard/> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  // @ is an alias to /src
+  // import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: "Home",
+    components: {
+      // HelloWorld
+    }
+  };
 </script>
+
+<style lang="scss">
+  .Home {
+    margin: 0 auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5%;
+	}
+</style>
